a383834338615755b99173656ea9f8f2e39cb7dd
victlop <127052704+victlop@users.noreply.github.com>
2023-07-20 18:14:06 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x6de4874a67e57cee76143fd8b9882c244279b756";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
